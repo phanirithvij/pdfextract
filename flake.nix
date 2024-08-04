3a5c2d77c1064910258c4428907321fcaa792848
phanirithvij <phanirithvij2000@gmail.com>
2024-08-04 14:08:40 +0530

    treefmt, deadnix, statix
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,22 +2,34 @@
   description = "A very basic pdf video extractor thing";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      treefmtCfg = (inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix).config.build;
     in
     {
       # TODO python deps, everything
       packages.${system} = {
-        hello = pkgs.hello;
+        inherit (pkgs) hello;
         default = self.packages.${system}.hello;
       };
-      formatter.${system} = pkgs.nixfmt-rfc-style;
+      formatter.${system} = treefmtCfg.wrapper;
+      checks.${system}.formatting = treefmtCfg.check self;
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [ (python3.withPackages (pp: [pp.pymupdf])) nixfmt-rfc-style ];
+        packages =
+          with pkgs;
+          [ (python3.withPackages (pp: [ pp.pymupdf ])) ]
+          ++ [
+            statix
+            deadnix
+            treefmtCfg.wrapper
+            (lib.attrValues treefmtCfg.programs)
+          ];
       };
     };
 }
